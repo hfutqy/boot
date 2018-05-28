da8da9a6590c1bcbc29a987997961f5fd6f02555
祁玉 <924120316@qq.com>
2018-05-28 13:58:56 +0800

    SpringBoot入门
    新增helloController

---
+++ src/main/java/com/qiyu/boot/controller/HelloController.java
@@ -0,0 +1,20 @@
+package com.qiyu.boot.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by qiyu on 2018/5/28.
+ *
+ * @RestController 是spring4里的新注解，是@ResponseBody和@Controller的缩写。
+ */
+@RestController
+public class HelloController {
+
+    @RequestMapping("/hello")
+    public String hello() {
+        return "Hello Spring Boot!";
+    }
+
+
+}
