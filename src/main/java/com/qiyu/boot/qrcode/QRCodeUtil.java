89a4ab32e93bb4e578b0b377db391dc3038211b8
祁玉 <924120316@qq.com>
2018-07-11 17:22:43 +0800

    111
    111

---
+++ src/main/java/com/qiyu/boot/qrcode/QRCodeUtil.java
@@ -0,0 +1,18 @@
+package com.qiyu.boot.qrcode;
+
+import com.github.binarywang.utils.qrcode.QrcodeUtils;
+
+import java.io.File;
+
+/**
+ * 二维码生成工具
+ */
+public class QRCodeUtil  {
+
+    public static void main(String[] args) {
+        File file = new File("xxxx");
+        QrcodeUtils.createQrcode("String",10,file);
+
+
+    }
+}
